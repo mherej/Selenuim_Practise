2293a71f9fb903ca11e24e2427de5838282898ae
mherej <marekherej2019@gmail.com>
2023-02-15 16:23:55 +0000

    Math Operations recap.

---
+++ Math/IntOperations.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Math
+{
+    [TestCategory("Math Operations")]
+    [TestClass]
+    public class IntOperations
+    {
+        // set vars
+        static int n1;
+        static int n2;
+    
+        [ClassInitialize()]
+        public static void IntInitalize(TestContext testContext)
+        {
+            n1 = 10;
+            n2 = 5;
+        } 
+
+        [TestMethod]
+        public void Test_Add()
+        {
+            int sum = n1 + n2;
+            Console.WriteLine(sum);
+            Assert.AreEqual(15, sum);
+        }
+        [TestMethod]
+        public void Test_Sub()
+        {
+        }
+        [TestMethod]
+        public void Test_Div()
+        {
+        }
+        [TestMethod]
+        public void Test_Multi()
+        {
+        }
+        [TestMethod]
+        public void Test_Modulus()
+        {
+        }
+    }
+}
---
+++ Math/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Math")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Math")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7b0d97a9-75b0-40a5-a317-969231d9e728")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
