ad97116b8b36239efd86dfc7147acddbdeb8453e
mherej <marekherej2019@gmail.com>
2023-02-17 12:52:01 +0000

    If else conditions recap.

---
+++ Math/If_elese_conditional_statements.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    [TestCategory("If else conditions")]
+    [TestClass]
+    public class If_elese_conditional_statements
+    {
+        static int n1, n2; 
+
+        [ClassInitialize()]
+        public static void IfElseConditionsTestContextInit(TestContext testContext)
+        {
+            n1 = 10; 
+            n2 = 5; 
+        }
+        [TestMethod]
+        public void n1_is_equal_n2()
+        {
+            if (n1 == n2)
+            {
+                Assert.IsTrue(n1 == n2);
+            }
+        }
+        [TestMethod]
+        public void n1_isnot_equal_n2()
+        {
+            if (n1 != n2)
+            {
+                Assert.IsTrue(n1 != n2);
+            }
+        }
+        [TestMethod]
+        public void n1_is_greater_than_n2()
+        {
+            if (n1 > n2)
+            {
+                Assert.IsTrue(n1 > n2);
+            }
+        }
+        [TestMethod]
+        public void n1_is_less_than_n2()
+        {
+            if (n1 < n2)
+            {
+                Assert.IsTrue(n1 < n2);
+            }
+        }
+    }
+}
