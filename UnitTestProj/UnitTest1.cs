be0cf8ce125980fac62bf6eff1017c0e484e1e7b
mherej <marekherej2019@gmail.com>
2023-02-21 12:34:04 +0000

    String Class Substring example added.

--- UnitTestProj/UnitTest1.cs
+++ UnitTestProj/UnitTest1.cs
@@ -3,6 +3,7 @@ using System;
 
 namespace UnitTestProj
 {
+    [TestCategory("General test Practise")]
     [TestClass]
     public class UnitTest1
     {
@@ -11,6 +12,20 @@ namespace UnitTestProj
         {
             int result = CL.Class1.Addition(2,3);
             Assert.AreEqual(6, result);
+        }
+    }
+
+    [TestCategory("String Class Practise")]
+    [TestClass]
+    public class UnitTest_String_Class
+    {
+        [TestMethod]
+        public void Using_string_class()
+        {
+            string myStrig = "I had a nice dearm today, I was in Mexico";
+            string newString = myStrig.Substring(0,12);
+
+            Console.WriteLine(newString);
         }
     }
 }
