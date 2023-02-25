62e3acf3e899daced7c6bfc3fe1f09b02fb2faf3
mherej <marekherej2019@gmail.com>
2023-02-25 12:46:31 +0000

    Trip class and test unit added.

---
+++ CL/Trip.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CL
+{
+    public class Trip
+    {
+        string destination { get; set; }
+        double distanceTravelled { get; set; }
+        double totalCostOfFuel { get; set; }
+        double numberOfLitersConsumed { get; set; }
+
+        public Trip(string dest, int distanceTrav, double totcost, double litersConsumend)
+        {
+            destination = dest;
+            distanceTravelled = distanceTrav;
+            totalCostOfFuel = totcost;
+            numberOfLitersConsumed = litersConsumend;
+        }
+
+        public double KilometersPerLiter(double kilometers, double liters)
+        {
+            return kilometers / liters;
+        }
+
+        public double CostPerKilometer(double cost, double disatanceInKilometers)
+        {
+            return cost / disatanceInKilometers;
+        }
+
+        public override string ToString()
+        {
+            return ($" Travellinmg to {destination} distsance {distanceTravelled} total cost of fuel {totalCostOfFuel} that is {numberOfLitersConsumed} fuel liters!");
+        }
+    } 
+}
---
+++ OOP_tests/Trip_tests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CL;
+
+namespace OOP_tests
+{
+    [TestCategory("Trip unit tests")]
+    [TestClass]
+    public class Trip_tests
+    {
+       static double totalCost;
+       static double totalDistance;
+       static double totalLiters;
+        [ClassInitialize()]
+        public static void TripCalssInit(TestContext testContext)
+        {
+            totalCost = 25.00;
+            totalDistance = 100.00;
+            totalLiters = 10.00;
+        }
+
+
+        [TestMethod]
+        public void Cost_Of_Trip()
+        {
+            // arrange
+            Trip trip1 = new Trip("Sligo", 25, 23.00, 3.4);
+            // act
+            var cost = trip1.CostPerKilometer( totalCost, totalDistance);
+            //assert
+            Assert.AreEqual(.25, cost);
+        }
+
+        [TestMethod]
+        public void Kilometers_Per_Liter()
+        {
+            // arrange
+            Trip trip1 = new Trip("Sligo", 25, 23.00, 3.4);
+            // act
+            var retult = trip1.KilometersPerLiter(totalDistance, totalLiters);
+            //assert
+            Assert.AreEqual(10, retult);
+        }
+
+    }
+}
