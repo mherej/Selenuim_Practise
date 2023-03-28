299beb7796ba8002ae314e8211debb1a0247636c
mherej <marekherej2019@gmail.com>
2023-03-28 10:45:32 +0100

    Property class setup WIP.

---
+++ CL/Property.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CL
+{
+    public class Property
+    {
+        string Address { get; set; }
+        decimal LastYearValue { get; set; }
+        decimal MillageRate { get; set; }
+
+
+        public Property(string address, decimal lastvalue)
+        {
+            Address = address;
+            LastYearValue = lastvalue;
+        }
+
+        public decimal CurrentAssessedValue(decimal value)
+        {
+            return value + (1*.027M);
+        }
+          
+        public decimal TaxableValue(decimal AssessedPropertyValue)
+        {
+            if (AssessedPropertyValue >= 25000)
+            {
+                return AssessedPropertyValue - 25000;
+            }
+            else
+            {
+                return AssessedPropertyValue;
+            }     
+        }
+
+        public decimal TaxesDue(decimal taxableValue)
+        {
+            return (taxableValue / 1000)*10.3M;
+        }
+
+
+    }
+}
