70aa02ee0561f4640b00d5d8d6a66b3b6ad25e8c
mherej <marekherej2019@gmail.com>
2023-04-03 12:07:12 +0100

    Person Student in heritance practise intro.

--- OOP_tests/OOP_Unit_test.cs
+++ OOP_tests/OOP_Unit_test.cs
@@ -4,92 +4,106 @@ using CL;
 
 namespace OOP_tests
 {
-    [TestCategory("Class tests")]
+    [TestCategory("Person/Student tests")]
     [TestClass]
-    public class OOP_Unit_test
+    public class Person_Student_UT
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Person_Student_class()
         {
-            // arrange
-            Car car = new Car(3, "red", false);
-            // act
-            // assert
-            Assert.AreEqual(car.Color, "red");
-        }
+            Person person = new Person("JHonny", "Bravo", 33);
+            Student student = new Student("Jenny", "Brown", 33, "34343", "Software development");
 
-        [TestMethod]
-        public void Car_Acceleration_test()
-        {
-            // arrange
-            Car car = new Car(3, "red", true);
-            // act
-            car.Accelerate();
-            // assert
-            Assert.AreEqual(car.Color, "red");
         }
 
 
-        [TestMethod]
-        public void Car__test()
+        [TestCategory("Class tests")]
+        [TestClass]
+        public class OOP_Unit_test
         {
-            // arrange
-            Car car = new Car(3, "red", true);
-            // act
-            bool isCarFamily = car.IsFamilityCar();
-            // assert
-            if(isCarFamily == true)
+            [TestMethod]
+            public void TestMethod1()
             {
-                Assert.AreEqual(car.IsFamilityCar(), true);
+                // arrange
+                Car car = new Car(3, "red", false);
+                // act
+                // assert
+                Assert.AreEqual(car.Color, "red");
             }
-            else
+
+            [TestMethod]
+            public void Car_Acceleration_test()
             {
-                Assert.Fail();
+                // arrange
+                Car car = new Car(3, "red", true);
+                // act
+                car.Accelerate();
+                // assert
+                Assert.AreEqual(car.Color, "red");
             }
-            
-        }
 
-        [TestMethod]
-        public void EmployeeNameCheck()
-        {
-            DateTime emp1StartDate = new DateTime(2012,01,01);
-            //arrange
-            Employee emp1 = new Employee(22, "Jonny", "Bravo", new DateTime(2012, 01, 01), "cashier", "sales", 2000.00M);
-            // act
-            // assert
-            Assert.AreEqual(emp1.FullName, "Jonny Bravo");
-        }
 
-        [TestMethod]
-        public void EmployeeNameSortCheck()
-        {
-            //arrange
-            Employee emp2 = new Employee(22, "Jonny", "Bravo", new DateTime(2012, 01, 01), "cashier", "sales", 2000.00M);
-            // act
-            // assert
-            Assert.AreEqual(emp2.SortableName, "Bravo, Jonny");
-        }
+            [TestMethod]
+            public void Car__test()
+            {
+                // arrange
+                Car car = new Car(3, "red", true);
+                // act
+                bool isCarFamily = car.IsFamilityCar();
+                // assert
+                if (isCarFamily == true)
+                {
+                    Assert.AreEqual(car.IsFamilityCar(), true);
+                }
+                else
+                {
+                    Assert.Fail();
+                }
 
-        [TestMethod]
-        public void Test_Employee_Constuctor()
-        {
-            //arrange
-            Employee emp2 = new Employee(1,"Jonhny", "Bravo","Manager");
-            // act
-            emp2.JobDescription = "Staff";
-            // assert
-            Assert.AreEqual(emp2.JobLocation(), "Chicago");
-        }
+            }
 
-        [TestMethod]
-        public void Test_Employee_ToSting()
-        {
-            //arrange
-            Employee emp2 = new Employee(1, "Jonhny", "Bravo", "Manager");
-            // act
-           Console.WriteLine(emp2);
-            // assert
-            //Assert.AreEqual(emp2.JobLocation(), "Chicago");
+            [TestMethod]
+            public void EmployeeNameCheck()
+            {
+                DateTime emp1StartDate = new DateTime(2012, 01, 01);
+                //arrange
+                Employee emp1 = new Employee(22, "Jonny", "Bravo", new DateTime(2012, 01, 01), "cashier", "sales", 2000.00M);
+                // act
+                // assert
+                Assert.AreEqual(emp1.FullName, "Jonny Bravo");
+            }
+
+            [TestMethod]
+            public void EmployeeNameSortCheck()
+            {
+                //arrange
+                Employee emp2 = new Employee(22, "Jonny", "Bravo", new DateTime(2012, 01, 01), "cashier", "sales", 2000.00M);
+                // act
+                // assert
+                Assert.AreEqual(emp2.SortableName, "Bravo, Jonny");
+            }
+
+            [TestMethod]
+            public void Test_Employee_Constuctor()
+            {
+                //arrange
+                Employee emp2 = new Employee(1, "Jonhny", "Bravo", "Manager");
+                // act
+                emp2.JobDescription = "Staff";
+                // assert
+                Assert.AreEqual(emp2.JobLocation(), "Chicago");
+            }
+
+            [TestMethod]
+            public void Test_Employee_ToSting()
+            {
+                //arrange
+                Employee emp2 = new Employee(1, "Jonhny", "Bravo", "Manager");
+                // act
+                Console.WriteLine(emp2);
+                // assert
+                //Assert.AreEqual(emp2.JobLocation(), "Chicago");
+            }
         }
     }
 }
---
+++ OOP_tests/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_tests
+{
+   public class Person
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        int Age { get; set; }
+
+        public Person (string fname, string lname, int age)
+        {
+            FirstName = fname;
+            LastName = lname;
+            Age = age;
+        }
+    }
+
+    public class Student : Person
+    {
+        string studenID { get; set; }
+        string Course { get; set; }
+
+        public Student(string fname, string lname, int age, string sid, string cour) : base(fname, lname, age)
+        {
+            studenID = sid;
+            Course = cour;
+        }
+    } 
+}
