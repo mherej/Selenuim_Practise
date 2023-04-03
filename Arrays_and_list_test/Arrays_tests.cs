60df95d0ddef1c42fba3ecde4f952c1830699017
mherej <marekherej2019@gmail.com>
2023-04-03 11:15:44 +0100

    ArrayList Practise.

--- Arrays_and_list_test/Arrays_tests.cs
+++ Arrays_and_list_test/Arrays_tests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -66,6 +67,84 @@ namespace Arrays_and_list_test
             return sum;
         }
 
+        [TestMethod]
+        public void Array_List_Testing()
+        {
+            ArrayList scores = new ArrayList();
+            fillthelist(scores);
+            Console.WriteLine(scores.Count);
 
+        }
+
+        private void fillthelist(ArrayList scores)
+        {
+            for(int i = 0; i <=20; i++)
+            {
+                scores.Add(i);
+            }
+        }
+
+        [TestMethod]
+        public void Array_List_Quiz()
+        {
+            ArrayList dataList = new ArrayList();
+
+            dataList.Add("Someday");
+            dataList.Add(2);
+            dataList.Add("app");
+            dataList.Add(13.5);
+            dataList.Add("red");
+            dataList.Add(8);
+            dataList.Add(99);
+            dataList.Add("6");
+            dataList.Add("Meatball soup");
+            dataList.Add(18M);
+            dataList.Add("pieces");
+            dataList.Add("14");
+
+           decimal  totaSumOfValidItems = SumOfItems(RemoveStringItems(dataList));
+
+           Console.WriteLine(totaSumOfValidItems);
+
+        }
+
+        public ArrayList RemoveStringItems(ArrayList listtosort)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            foreach (var item  in listtosort)
+            {
+                if (item is int)
+                {
+                    arrayList.Add(item);
+                }
+                else if (item is double)
+                {
+                    arrayList.Add(item);
+                }
+                else if (item is decimal)
+                {
+                    arrayList.Add(item);
+                }
+            }
+
+            return arrayList;
+        }
+
+        public decimal SumOfItems(ArrayList list)
+        {
+            ArrayList sumal = new ArrayList();
+            decimal sum = 0M;
+
+            foreach (var item in list)
+            {
+                if (item is int || item is float || item is double || item is decimal || item is long)
+                {
+                    sum += Convert.ToDecimal(item);
+                }
+            }
+
+            return sum;
+        }
     }
 }
